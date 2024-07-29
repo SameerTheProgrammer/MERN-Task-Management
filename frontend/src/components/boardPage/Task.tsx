8a27a28da4ee313e443005ba3c35ad0c9c27367a
Sameer kumar <2006Sameer2009@gmail.com>
2024-07-29 15:12:54 +0530

    task component design complete

--- frontend/src/components/boardPage/Task.tsx
+++ frontend/src/components/boardPage/Task.tsx
@@ -1,10 +1,21 @@
-import { Box, IconButton, ScaleFade } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  Flex,
+  Heading,
+  Icon,
+  IconButton,
+  ScaleFade,
+  Text,
+} from "@chakra-ui/react";
 import _ from "lodash";
 import { memo } from "react";
-import { AutoResizeTextarea } from "./AutoResizeTextArea";
+// import { AutoResizeTextarea } from "./AutoResizeTextArea";
 import { TaskModel } from "../../utils/models";
 import { useTaskDragAndDrop } from "../../hooks/useTaskDragAndDrop";
 import { MdDeleteOutline } from "react-icons/md";
+import { FaRegClock } from "react-icons/fa6";
+import { GoPencil } from "react-icons/go";
 
 type TaskProps = {
   index: number;
@@ -48,11 +59,12 @@ function Task({
         pr={7}
         pt={3}
         pb={1}
-        boxShadow="xl"
         cursor="grab"
-        fontWeight="bold"
         userSelect="none"
-        bgColor={task.color}
+        // bgColor={task.color}
+        bgColor={"gray.50"}
+        borderWidth="2px"
+        borderColor={"gray.200"}
         opacity={isDragging ? 0.5 : 1}
       >
         <IconButton
@@ -61,7 +73,7 @@ function Task({
           right={0}
           zIndex={100}
           aria-label="delete-task"
-          size="md"
+          size="lg"
           colorScheme="solid"
           color={"gray.700"}
           icon={<MdDeleteOutline />}
@@ -71,7 +83,23 @@ function Task({
           }}
           onClick={handleDeleteClick}
         />
-        <AutoResizeTextarea
+        <IconButton
+          position="absolute"
+          top={5}
+          right={0}
+          zIndex={100}
+          aria-label="delete-task"
+          size="lg"
+          colorScheme="solid"
+          color={"gray.700"}
+          icon={<GoPencil />}
+          opacity={0}
+          _groupHover={{
+            opacity: 1,
+          }}
+          onClick={handleDeleteClick}
+        />
+        {/* <AutoResizeTextarea
           value={task.title}
           fontWeight="semibold"
           cursor="inherit"
@@ -83,7 +111,41 @@ function Task({
           focusBorderColor="none"
           color="gray.700"
           onChange={handleTitleChange}
-        />
+        /> */}
+        <Flex direction={"column"} rowGap={"8px"}>
+          <Heading color={"gray.500"} as={"h4"} size={"sm"}>
+            Implement User Authentication
+          </Heading>
+          <div>
+            <Text fontSize="sm" color={"gray.500"}>
+              Develop and integrate user authentication using email and password
+            </Text>
+          </div>
+          <Flex w={"100%"} justifyContent={"space-between"}>
+            <Badge
+              borderRadius={5}
+              color={"gray.600"}
+              px={4}
+              py={2}
+              w={"max-content"}
+              bgColor="gray.300"
+            >
+              Low
+            </Badge>
+            <Flex
+              alignItems={"center"}
+              columnGap={"10px"}
+              color={"gray.500"}
+              fontWeight={"bold"}
+            >
+              <Icon boxSize={4} as={FaRegClock} />
+              <Text fontSize={"13px"}>2024-07-31</Text>
+            </Flex>
+          </Flex>
+          <Text color={"gray.500"} fontWeight={"bold"} fontSize={"sm"}>
+            1 hr ago
+          </Text>
+        </Flex>
       </Box>
     </ScaleFade>
   );
